9cef6f8e022fd08fb2d3970596b137135977f86f
Ryan Doyle <ryan@doylenet.net>
2016-08-02 22:30:08 +1000

    Add tests for pmapi

---
+++ pmapi/pmapi_test.go
@@ -0,0 +1,67 @@
+package pmapi
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestPmapiContext_PmGetContextHostname(t *testing.T) {
+	c, _ := PmNewContext(PmContextHost, "localhost")
+
+	assertEquals(t, c.PmGetContextHostname(), "ryanlap")
+}
+
+func TestPmNewContext_withAnInvalidHostHasANilContext(t *testing.T) {
+	c, _ := PmNewContext(PmContextHost, "not-a-host")
+
+	assertNil(t, c)
+}
+
+func TestPmNewContext_withAnInvalidHostHasAnError(t *testing.T) {
+	_, err := PmNewContext(PmContextHost, "not-a-host")
+
+	assertNotNil(t, err)
+}
+
+func TestPmNewContext_hasAValidContext(t *testing.T) {
+	c, _ := PmNewContext(PmContextHost, "localhost")
+
+	assertNotNil(t, c)
+}
+
+func TestPmNewContext_hasANilError(t *testing.T) {
+	_, err := PmNewContext(PmContextHost, "localhost")
+
+	assertNil(t, err)
+}
+
+func TestPmNewContext_supportsALocalContext(t *testing.T) {
+	c, _ := PmNewContext(PmContextLocal, "")
+
+	assertNotNil(t, c)
+}
+
+func assertEquals(t *testing.T, a interface{}, b interface{}) {
+	if(a != b) {
+		t.Errorf("expected %v, got %v", b, a)
+	}
+}
+
+func assertNil(t *testing.T, a interface{}) {
+	if ! isNil(a) {
+		t.Errorf("expected nil but got %v", a)
+	}
+}
+
+func assertNotNil(t *testing.T, a interface{}) {
+	if isNil(a) {
+		t.Errorf("expected not nil but got %v", a)
+	}
+}
+
+func isNil(object interface{}) bool {
+	if object == nil {
+		return true
+	}
+	return reflect.ValueOf(object).IsNil()
+}
